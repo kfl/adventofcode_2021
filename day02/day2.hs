ffb906437beb751648f62b6d724d458432c9c9ca
Ken Friis Larsen <ken@friislarsen.net>
2021-12-02 12:10:00 +0100

    Haskell solution for day 2
    Failed to make a nice data parallel solution for part2

---
+++ day02/day2.hs
@@ -0,0 +1,69 @@
+import Data.List
+
+test = map parse ["forward 5"
+                 , "down 5"
+                 , "forward 8"
+                 , "up 3"
+                 , "down 8"
+                 , "forward 2"
+                 ]
+
+input = do
+  content <- readFile "input.txt"
+  return $ map parse $ lines content
+
+data Command = Forward Int
+             | Down Int
+             | Up Int
+             deriving (Eq, Show)
+
+parse_command str =
+  case str of
+    "forward" -> Forward
+    "down" -> Down
+    "up" -> Up
+
+parse line = parse_command w1 $ read w2
+  where [w1, w2] = words line
+
+data Pos = Pos !Int !Int
+  deriving (Show)
+
+instance Semigroup Pos where
+  (Pos x y) <> (Pos a b) = Pos (x+a) (y+b)
+
+instance Monoid Pos where
+  mempty = Pos 0 0
+
+effect (Forward x) = Pos x 0
+effect (Down x) = Pos 0 x
+effect (Up x) = Pos 0 (-x)
+
+part1 commands = hori * vert
+  where Pos hori vert = foldMap effect commands
+
+answer1 = part1 <$> input
+
+data Aim = Aim !Int !Int !Int
+  deriving (Show)
+
+---- Failed attempt at making a semigroup
+instance Semigroup Aim where
+  (Aim x y z) <> (Aim a b c) = Aim (x+a) (y+z*b) (z+c)
+
+instance Monoid Aim where
+  mempty = Aim 0 0 0
+
+effect2 (Forward x) = Aim x x 0
+effect2 (Down x) = Aim 0 1 x
+effect2 (Up x) = Aim 0 1 (-x)
+---- end
+
+update (Aim h v a) (Forward x) = Aim (h+x) (v+x*a) a
+update (Aim h v a) (Down x) = Aim h v (a+x)
+update (Aim h v a) (Up x) = Aim h v (a-x)
+
+part2 commands = hori * vert
+  where Aim hori vert aim = foldl' update mempty commands
+
+answer2 = part2 <$> input
