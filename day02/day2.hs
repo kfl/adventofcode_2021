3f7638341deca095026c0d8b2a8231d1c527c055
Ken Friis Larsen <ken@friislarsen.net>
2021-12-02 16:26:29 +0100

    Add QuickCheck tests to document that I'm lying wrt. semigroups and monoids

--- day02/day2.hs
+++ day02/day2.hs
@@ -1,5 +1,11 @@
+module Day2 where
+
 import Data.List
 import Data.Foldable (foldMap')
+import qualified Test.QuickCheck as QC
+import Test.QuickCheck ((===))
+import qualified Test.QuickCheck.Checkers as QC
+import qualified Test.QuickCheck.Classes as QC
 
 test = map parse ["forward 5"
                  , "down 5"
@@ -28,7 +34,7 @@ parse line = parse_command w1 $ read w2
   where [w1, w2] = words line
 
 data Pos = Pos !Int !Int
-  deriving (Show)
+  deriving (Show, Eq)
 
 instance Semigroup Pos where
   (Pos x y) <> (Pos a b) = Pos (x+a) (y+b)
@@ -46,9 +52,9 @@ part1 commands = hori * vert
 answer1 = part1 <$> input
 
 data Aim = Aim !Int !Int !Int
-  deriving (Show)
+  deriving (Show, Eq)
 
-instance Semigroup Aim where
+instance Semigroup Aim where -- FIXME not really a semigroup, see QuickCheck tests for documentation
   (Aim h v a) <> (Aim x y z) = Aim (h+x) (v+y*a) (a+z)
 
 instance Monoid Aim where -- FIXME not really a monoid
@@ -69,3 +75,28 @@ part2 commands = hori * vert
   where Aim hori vert aim = foldl' update mempty commands
 
 answer2 = part2 <$> input
+
+
+-- Documenting my shameful unlawfulness (see FIXMEs above)
+
+instance QC.Arbitrary Pos where
+  arbitrary = Pos <$> QC.arbitrary <*> QC.arbitrary
+  shrink (Pos h v) = [ Pos h' v' | (h', v') <- QC.shrink (h,v) ]
+
+instance QC.EqProp Pos where
+  x =-= y = x === y
+
+instance QC.Arbitrary Aim where
+  arbitrary = Aim <$> QC.arbitrary <*> QC.arbitrary <*> QC.arbitrary
+  shrink (Aim h v a) = [ Aim h' v' a' | (h', v', a') <- QC.shrink (h,v,a) ]
+
+instance QC.EqProp Aim where
+  x =-= y = x === y
+
+shame = do
+  -- These holds
+  QC.quickBatch $ QC.semigroup (mempty :: Pos, 5 :: Int)
+  QC.quickBatch $ QC.monoid (mempty :: Pos)
+  -- These don't hold
+  QC.quickBatch $ QC.semigroup (mempty :: Aim, 5 :: Int)
+  QC.quickBatch $ QC.monoid (mempty :: Aim)
